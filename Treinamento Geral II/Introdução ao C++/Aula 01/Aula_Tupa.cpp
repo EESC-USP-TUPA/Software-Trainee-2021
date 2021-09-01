25b1c96c7dd62d35e38a4d0e9e5914376348ec00
LuccaMonzani <lucca.monzani@gmail.com>
2021-08-31 21:33:07 -0300

    adição de códigos aula 1

---
+++ Treinamento Geral II/Introdução ao C++/Aula 01/Aula_Tupa.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include "Cabecalho.h"
+
+using namespace std;
+
+//int x = 1, y = 0, z = 0;
+
+
+int main() {
+	//Variáveis:
+
+	//cout << x << endl;
+	//cout << &x << endl;
+	//x = 2;
+	//cout << x << endl;
+	//cout << &x << endl;
+
+	//Condicional:
+
+	//if (!x == y) { cout << "Tupa eh a melhor equipe de carro eletrico da USP" << endl; };
+
+	//if ((y == z || x == y) && x == z) { cout << "Tupa eh a melhor equipe de carro eletrico da USP" << endl; };
+
+	/*if (x == y && y == z){ 
+		cout << "Tupa n eh a melhor equipe de carro eletrico da terra" << endl; 
+	}
+	else{ 
+		cout << "Tupa eh a melhor equipe de carro eletrico da terra" << endl; 
+	}*/
+
+	/*(x > y) ? x = 1: x = 0;
+	cout << x << endl;*/
+
+	//Laços:
+
+	//int i;
+	//for (i = 0; i < 10; i++)
+	//{
+	//	x += 1;
+	//	cout << x << endl;
+	//}
+
+	//while (x<10){
+	//	cout << "x < 10, continue inserindo valores" << endl;
+	//	cout << "x = ";
+	//	cin >> x;
+	//	if (x >= 10) {
+	//		cout << "x > 10, programa encerrado" << endl;
+	//	}
+	//}
+
+	//while (x<10){
+	//	x += 1;
+	//}
+	//cout << x << endl;
+
+	//do{
+	//	x += 1;
+	//} while (x<10);
+	//cout << x << endl;
+}
---
+++ Treinamento Geral II/Introdução ao C++/Aula 01/Cabecalho.h
@@ -0,0 +1,6 @@
+#include <iostream>
+using namespace std;
+
+int x = 1, y = 0, z = 0;
+float f = 5.21;
+double d = 6.1239812803;
